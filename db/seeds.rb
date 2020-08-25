1de4180b9bdd9c8247e6562b868d05c1dcff450b
Victorien Brun <b.victorien@hotmail.fr>
2020-08-25 13:50:16 +0200

    create controller and view ANIMALS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clean DB'
+Animal.destroy_all
+
+
+puts 'index some animals'
+a = Animal.create!(common_name: 'crevette pistolet tigre', latin_name: 'Alpheus bellulus', description: "La crevette pistolet tigre a la forme d'un petit homard avec un corps blanc strié de bandes marron orangé tachetées de blanc.")
+Animal.create!(common_name: 'dauphin commun', latin_name: 'Delphinus delphis', description: "Le dauphin commun est gris foncé à noir sur le dos, blanc jaunâtre sur les flancs. Il a un dessin en forme de sablier et une ligne foncé allant de la nageoire pectorale au bec.")
+Animal.create!(common_name: 'baleine à bosse', latin_name: 'Megaptera novaeangliae', description: "Le dessus du corps des baleines à bosse est entièrement noir, le dessous est plutôt blanchâtre. La tête et la mâchoire inférieure sont couvertes de petites protubérances appelées tubercules. Les nageoires pectorales, noires et blanches, peuvent atteindre jusqu'au tiers de la longueur du corps. ")
+Animal.create!(common_name: 'raie pastenague épineuse', latin_name: 'Dasyatis centroura ', description: "La raie pastenague épineuse a le corps en forme de losange arrondi de couleur gris clair à foncé ou gris olivâtre. Des tubercules sont présents sur le dessus du corps. Sa tête a un museau pointu et de petits yeux.")
+
+
+puts 'finished'
