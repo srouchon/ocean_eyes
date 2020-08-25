62304bd7a872eedbfded730b2b088e6686475026
Deva Sou <soudeva974@gmail.com>
2020-08-25 14:41:03 +0200

    adding request from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "uri"
+require "net/http"
+
+@response_body = {}
+
+url = URI("https://fishbase.ropensci.org/species?&limit=5")
+
+https = Net::HTTP.new(url.host, url.port);
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+
+@response_body = response.read_body
+
+byebug
+
+
+
+# iterer sur response_body
+# aller sur les pages suivantes
+# dans array, liste species
