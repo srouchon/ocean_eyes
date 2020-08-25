4581e0dd9b81a88101219fc5a3088b97bbbef9a2
Victorien Brun <b.victorien@hotmail.fr>
2020-08-25 22:45:51 +0200

    create-search-bar with PG SEARCH

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,11 @@
 class Animal < ApplicationRecord
   has_many :subregions, through: :habitats
   has_many :users, through: :favorites
+
+  include PgSearch::Model
+  pg_search_scope :search_by_common_name_by_latin_name_and_description,
+    against: [ :common_name, :latin_name, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
