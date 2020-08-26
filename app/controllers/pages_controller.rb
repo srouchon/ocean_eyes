f0e854863afcdec632245d68b41f1b84f02c57e6
Ophelie Roux <ophelie.roux@gmail.com>
2020-08-26 14:47:45 +0200

    no longer need to login for the animal#index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-
+  skip_before_action :authenticate_user!, only: :animals
   def home
   end
 
   def associations
   end
-  
+
   def thanks
   end
 end
